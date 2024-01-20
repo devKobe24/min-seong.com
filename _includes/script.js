88ae923ef50a63128f3da8b6f7714d186ad8aafb
kobe <dev.skyachieve91@gmail.com>
2024-01-21 07:15:51 +0900

    :sparkles: Feat: 스크롤 이동 시 헤더 영역에 스타일 적용.

--- _includes/script.js
+++ _includes/script.js
@@ -28,3 +28,19 @@
   writeTxt();
 })();
 /* end text_iife.js */
+
+/* 수직 스크롤이 발생하면 header 태그에 active 클래스 추가 및 삭제 */
+/* scroll_request.js */
+const headerEl = document.querySelector("header");
+window.addEventListener('scroll', function(){
+  this.requestAnimationFrame(scrollCheck);
+})
+function scrollCheck() {
+  let browerScrollY = window.scrollY ? window.scrollY : window.pageYOffset;
+  if(browerScrollY > 0){
+    headerEl.classList.add("active");
+  }else{
+    headerEl.classList.remove("active");
+  }
+}
+/* end scroll_request.js */
